33a3ddd8ffed0ea6481c95c32fab028fab13b470
Marisha <7513541@gmail.com>
2023-05-11 11:17:00 +0300

    host images

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const uploadRouter = require('./routes/upload.router')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('public'))
 
 const apiRouter = Router()
 
