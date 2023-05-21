3693525e0cdf3d75523577da33f3a23ad62a0074
Marisha <7513541@gmail.com>
2023-05-21 20:14:47 +0300

    fix cors

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const bookingRouter = require('./routes/booking.router')
 const uploadRouter = require('./routes/upload.router')
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    credentials: true
+}))
 app.use(express.json())
 app.use(express.static('public'))
 
