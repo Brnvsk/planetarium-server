b2cd43cc24d6ee93e936e933fc49107e039d4b55
Marisha <7513541@gmail.com>
2023-05-21 23:49:11 +0300

    fix upload

--- config/multer.config.js
+++ config/multer.config.js
@@ -1,3 +1,4 @@
+const { existsSync, mkdirSync } = require('fs');
 const multer = require('multer')
 const path = require('path')
 
@@ -6,14 +7,20 @@ const storage = multer.diskStorage({
     // Choose a destination
     destination: function(req, file, cb) {
         const { path } = req.body
-            // cb(null, `${__dirname}/tmp/${path}`);
-            // console.log(file);
-        cb(null, `./tmp/${path}`);
+        const folder = `./tmp/${path}`
+        console.log(folder);
+        if (!existsSync(folder)) {
+            console.log('create');
+            mkdirSync(folder)
+        }
+
+        cb(null, folder);
     },
 
     // Choose a filename for each uploaded image
     filename: function(req, file, cb) {
         const fixedOriginalName = Buffer.from(file.originalname, 'latin1').toString('utf8')
+        console.log('fixedOriginalName', fixedOriginalName);
 
         cb(null, fixedOriginalName);
     },
